3e81bf324260ec934d0dbf381d2353bee510c1f5
Jimmy LAU <cappdo30@swd.gov.hk>
2022-12-05 11:03:12 +0800

    project init

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import VueLuckyCanvas from '@lucky-canvas/vue'
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './assets/main.css'
+
+createApp(App).use(VueLuckyCanvas).mount('#app')
