36a39c5414b7eaa1016c3704f827ab496b8492e6
Jimmy LAU <cappdo30@swd.gov.hk>
2022-12-05 11:18:06 +0800

    Restructure folder for Github page

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import VueLuckyCanvas from '@lucky-canvas/vue'
-import { createApp } from 'vue'
-import App from './App.vue'
-
-import './assets/main.css'
-
-createApp(App).use(VueLuckyCanvas).mount('#app')
